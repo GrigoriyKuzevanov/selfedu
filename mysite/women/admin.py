9a2ace63760daaf63199e4d089d73db3c294786c
Григорий Кузеванов <grkuzevanov@gmail.com>
2023-08-14 10:06:32 +0500

    add slug fields for models Women, Category

--- mysite/women/admin.py
+++ mysite/women/admin.py
@@ -9,6 +9,7 @@ class WomenAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')  # поля для поиска
     list_editable = ('is_published',)  # поля с возможностью редактирования в админпанели
     list_filter = ('is_published', 'time_create')  # поля для фильтрации
+    prepopulated_fields = {'slug': ('title',)}  # поля для автоматического заполнения по выбранному полю
 
 
 # @admin.register(Category)
@@ -16,6 +17,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
 
 
 admin.site.register(Women, WomenAdmin)
